fee8a3d173d7e609a26e6b5336c559a84eb455ee
GANESH2202 <gtawde2202@gmail.com>
2022-09-16 00:00:21 +0530

    initial

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const fs = require('fs')
+
+const app = express()
+
+const port = 5000;
+
+
+
+app.get('/student/studentList',(req,res) =>{
+    fs.readFile('./API/user.json',"utf-8",(err,data) =>{
+        
+        if(err){
+             
+            console.log(err);
+        }
+
+        res.end(data);
+  });
+});
+
+
+app.listen(port,()=>{
+    console.log(`listening at http://localhost:${port}`)
+})
